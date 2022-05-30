3b45dd150b56897d7a40f9ade0b93fa595eed83d
Sara Ibarra <saraiba33@gmail.com>
2022-05-30 13:30:54 -0600

    Move scanners into methods

--- App.java
+++ App.java
@@ -2,33 +2,59 @@ import java.util.Scanner;
 
 class App {
   public static void main(String args[]) {
-    Scanner carRental = new Scanner(System.in);
 
-    System.out.println(
-        "Pickup Date (ex: 05/24/22), Would you like Toll tag for $3.95/day? (yes/no), Would you like a gps for $2.95/day? (yes/no), Would you like roadside assistance for $3.95/day? (yes/no), Customers Age, How many days will you need the rental?");
-
-    String pickUpDate = carRental.nextLine();
-    String tollTag = carRental.nextLine();
-    String gpsRental = carRental.nextLine();
-    String roadsideAssistance = carRental.nextLine();
-
-    int customersAge = carRental.nextInt();
-    int lengthOfRental = carRental.nextInt();
-
-    double totalToll = checkToll(tollTag, lengthOfRental);
-    double totalGps = checkGps(gpsRental, lengthOfRental);
-    double totalRoadside = checkRoadside(roadsideAssistance, lengthOfRental);
-    double totalSurcharge = surchargeCalculated(customersAge, lengthOfRental);
+    double totalToll = checkToll(tollResponse(), lengthResponse());
+    double totalGps = checkGps(gpsResponse(), lengthResponse());
+    double totalRoadside = checkRoadside(roadsideResponse(), lengthResponse());
+    double totalSurcharge = surchargeCalculated(AgeResponse(), lengthResponse());
     double totalOptions = optionsCalculated(totalToll, totalGps, totalRoadside);
-    double totalBase = getBaseRental(lengthOfRental);
-
-    System.out.println("The rental starts on " + pickUpDate);
+    double totalBase = getBaseRental(lengthResponse());
+    System.out.println("The rental starts on " + dateReponse());
     System.out.println("Basic Rent fee: $" + String.format("%.2f", totalBase));
     System.out.println("Options cost: $" + String.format("%.2f", totalOptions));
-    System.out.println("Under age surcharge: $" + String.format("%.2f", totalSurcharge));
+    System.out.println("Under age surcharge: $" + String.format("%.2f",
+        totalSurcharge));
     System.out.println(
-        "The total rental cost is: $" + String.format("%.2f", getRentalTotal(totalBase, totalOptions, totalSurcharge)));
+        "The total rental cost is: $" + String.format("%.2f",
+            getRentalTotal(totalBase, totalOptions, totalSurcharge)));
 
+  }
+
+  private static String dateReponse() {
+    Scanner pickUpScanner = new Scanner(System.in);
+    System.out.println("Pickup Date (ex: 05/24/22)");
+    return pickUpScanner.nextLine();
+  }
+
+  private static String tollResponse() {
+    Scanner tollScanner = new Scanner(System.in);
+    System.out.println("Would you like Toll tag for $3.95/day? (yes/no)");
+    return tollScanner.nextLine();
+  }
+
+  private static String gpsResponse() {
+    Scanner gpsScanner = new Scanner(System.in);
+    System.out.println("Would you like a gps for $2.95/day? (yes/no)");
+    return gpsScanner.nextLine();
+
+  }
+
+  private static String roadsideResponse() {
+    Scanner roadsideScanner = new Scanner(System.in);
+    System.out.println("Would you like roadside assistance for $3.95/day? (yes/no)");
+    return roadsideScanner.nextLine();
+  }
+
+  private static int AgeResponse() {
+    Scanner ageScanner = new Scanner(System.in);
+    System.out.println("Customers Age");
+    return ageScanner.nextInt();
+  }
+
+  private static int lengthResponse() {
+    Scanner lenghtScanner = new Scanner(System.in);
+    System.out.println("How many days will you need the rental?");
+    return lenghtScanner.nextInt();
   }
 
   public static double dailyBase = 29.99;
@@ -42,7 +68,8 @@ class App {
   }
 
   public static double optionsCalculated(double toll, double gps, double roadside) {
-    return toll += gps += roadside;
+    double tollAndGps = toll += gps;
+    return tollAndGps += roadside;
   }
 
   public static double checkToll(String toll, int days) {
@@ -79,6 +106,7 @@ class App {
   }
 
   public static double getRentalTotal(double base, double options, double surcharge) {
-    return base += options += surcharge;
+    double baseAndOptions = base += options;
+    return baseAndOptions += surcharge;
   }
 }
